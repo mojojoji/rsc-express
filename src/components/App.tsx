39f91f37719f3d170c54b27f5a17bccf0e1fa518
Joji Augustine <jojimail@gmail.com>
2024-06-20 16:18:02 +0200

    working suspense

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,23 @@
-export function App() {
+import { Suspense } from 'react';
+
+export async function DelayedComponent() {
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+	return <div>Delayed component</div>;
+}
+
+export async function App() {
 	return (
 		<html lang="en">
 			<head>
 				<title>Document</title>
 			</head>
-			<body>Test react server component</body>
+			<body>
+				<h1>Test react server component</h1>
+				<Suspense fallback={<div>Loading...</div>}>
+					{/* @ts-expect-error Async Server Component */}
+					<DelayedComponent />
+				</Suspense>
+			</body>
 		</html>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+	/* @ts-expect-error Async Server Component */
 	const { pipe } = renderToPipeableStream(<App />, {
 		bootstrapScripts: ['/main.js'],
 		onShellReady() {
