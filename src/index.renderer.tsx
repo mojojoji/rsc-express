a49ee4302f34f7b0ab58e163076fc86f79d892f5
Joji Augustine <jojimail@gmail.com>
2024-06-24 23:46:28 +0200

    move to node streams

--- src/index.renderer.tsx
+++ src/index.renderer.tsx
@@ -1,15 +1,36 @@
 import express from 'express';
+import http from 'http';
 
 /* @ts-expect-error No types for package */
-import ReactServerDomWebpack from 'react-server-dom-webpack/client.edge';
+import ReactServerDomWebpack from 'react-server-dom-webpack/client.node';
 import { renderToPipeableStream } from 'react-dom/server';
 
 const app = express();
 const port = 3001;
 
+function request(options: http.RequestOptions, body: http.IncomingMessage): Promise<http.IncomingMessage> {
+	return new Promise((resolve, reject) => {
+		const req = http.request(options, (res) => {
+			resolve(res);
+		});
+		req.on('error', (e) => {
+			reject(e);
+		});
+		body.pipe(req);
+	});
+}
+
 app.get('/', async (req, res) => {
-	const fetchResponse = fetch('http://localhost:3000');
-	const result = await ReactServerDomWebpack.createFromFetch(fetchResponse, {
+	const stream = await request(
+		{
+			host: 'localhost',
+			port: 3000,
+			method: req.method,
+			path: '/',
+		},
+		req,
+	);
+	const result = await ReactServerDomWebpack.createFromNodeStream(stream, {
 		ssrManifest: {},
 	});
 	const { pipe } = renderToPipeableStream(result);
